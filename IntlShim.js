934e486b5e5929c5b7b5aa6a44c90c3092d9a92f
JCEmmons <emmo@us.ibm.com>
2014-04-14 13:20:00 -0500

    Simplifying the shim.

--- IntlShim.js
+++ IntlShim.js
@@ -1,14 +1,13 @@
 //
 // Shim for native vs. ecma402 package implementation of the Intl object.
 //
-var __globalObject = (function(){return this;});
 var dependencies = [];
-if (__globalObject.Intl === undefined) {
+if (Intl === undefined || Intl.NumberFormat === undefined || Intl.DateTimeFormat === undefined) {
 	dependencies.push("./Intl");
 }
-define(dependencies, function (Intl) {
-	if (__globalObject.Intl !== undefined) {
-		return __globalObject.Intl;
+define(dependencies, function (ecma402Intl) {
+	if (Intl === undefined || Intl.NumberFormat === undefined || Intl.DateTimeFormat === undefined) {
+		return ecma402Intl;
 	}
 	return Intl;
 });
