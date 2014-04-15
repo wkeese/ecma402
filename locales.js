7cf2f81af7e155f6b2c69b30dc53a497fbc00e5c
Bill Keese <bill@dojotoolkit.org>
2014-04-16 04:04:47 +0900

    more fixes to plugin

--- locales.js
+++ locales.js
@@ -1,73 +1,62 @@
 /**
- * Plugin to preload the locales the app has specified
+ * Plugin to pre-load the locales the app has specified via require.config()
  */
 
 define(["module", "require", "./common"], function (module, require, common) {
 
+
+	// Compute locales to pre-load, and start building data structure this plugin will return
+	// (a hash with a key for each pre-load locale).
+	var localeData = {
+		root: {}
+	};
+	localeData[common.DefaultLocale()] = {};
+
+	// Add each locale specified in this module's configuration.
 	var config = module.config();
-
-	var jsonElements = [ "currencies", "numbers", "ca-gregorian" ];
-
-	// Compute list of locales to pre-load, based on app's configuration settings for this module
-	var preLoadList = [ "root", common.DefaultLocale() ];
-	if (typeof config === "string") {
-		if (config === "allAvailable") {	// TODO: get rid of this?  It's redundant with the /.*/ regexp.
-			common.availableLocalesList.forEach(function (locale) {
-				if (preLoadList.indexOf(locale) === -1) {
-					preLoadList.push(locale);
-				}
+	if (config) {
+		if (typeof config === "string") {
+			config = [ config ];
+		} else if (config instanceof RegExp) {
+			config = common.availableLocalesList.filter(function (locale) {
+				return config.test(locale);
 			});
-		} else {
-			var bestFitPreload = common.BestFitAvailableLocale(common.availableLocalesList, config);
-			if (bestFitPreload && preLoadList.indexOf(bestFitPreload) === -1) {
-				preLoadList.push(bestFitPreload);
-			}
 		}
-	} else if (config instanceof Array) {
 		config.forEach(function (locale) {
 			var bestFitPreload = common.BestFitAvailableLocale(common.availableLocalesList, locale);
-			if (bestFitPreload && preLoadList.indexOf(bestFitPreload) === -1) {
-				preLoadList.push(bestFitPreload);
-			}
-		});
-	} else if (config instanceof RegExp) {
-		common.availableLocalesList.forEach(function (locale) {
-			if (config.test(locale) && preLoadList.indexOf(locale) === -1) {
-				preLoadList.push(locale);
+			if (bestFitPreload) {
+				localeData[bestFitPreload] = {};
 			}
 		});
 	}
 
+	// Array of pre-load locales
+	var locales = Object.keys(localeData);
+
 	// Compute dependencies to require() before this plugin resolves:
 	// For every locale, load "currencies", "numbers", and "ca-gregorian".
+	var jsonElements = [ "currencies", "numbers", "ca-gregorian" ];
 	var dependencies = [];
-	preLoadList.forEach(function (locale) {
+	locales.forEach(function (locale) {
 		jsonElements.forEach(function (element) {
 			dependencies.push("requirejs-text/text!./cldr/" + locale + "/" + element + ".json");
 		});
 	});
 
-
 	return {
 		load: function (path, require, onload) {
-
 			require(dependencies, function () {
 				var deps = Array(arguments);
 
-				// Return value from this plugin.
-				var locales = {
-				};
-
-				// Now that we've loaded everything all the JSON files we need,
-				// stick the results into the locales hash.
-				preLoadList.forEach(function (locale) {
-					var l = locales[locale] = {};
+				// Now that we've loaded all the JSON files we need, stick the results into the localeData hash.
+				locales.forEach(function (locale) {
+					var l = localeData[locale];
 					jsonElements.forEach(function (element) {
 						l[element] = JSON.parse(deps.shift());
 					});
 				});
 
-				onload(locales);
+				onload(localeData);
 			});
 		}
 	};
