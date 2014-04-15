d7dc5fc547ed05b31c34aac222ca6f78fa804118
Bill Keese <bill@dojotoolkit.org>
2014-04-16 02:44:04 +0900

    Fix locale preloading.
    Locales.js is now a plugin that preloads all the specified locales, and returns a hash
    with those locales.

--- locales.js
+++ locales.js
@@ -1,54 +1,74 @@
 /**
- * 
+ * Plugin to preload the locales the app has specified
  */
-define( [ "module", "./common", "../requirejs/require" ], function (module, common, require) {
-	var locales = {
-		preLoadList : [ "root" ],
-		jsonElements : [ "currencies", "numbers", "ca-gregorian" ]
-	};
 
-	function loadLocaleElements (locale) {
-		locales.jsonElements.forEach(function (element) {
-			var dependency = "../requirejs-text/text!cldr/" + locale + "/" + element + ".json";
-			this.require([ dependency ]);
+define(["module", "require", "./common"], function (module, require, common) {
+
+	var config = module.config();
+
+	var jsonElements = [ "currencies", "numbers", "ca-gregorian" ];
+
+	// Compute list of locales to pre-load, based on app's configuration settings for this module
+	var preLoadList = [ "root", common.DefaultLocale() ];
+	if (typeof config === "string") {
+		if (config === "allAvailable") {	// TODO: get rid of this?  It's redundant with the /.*/ regexp.
+			common.availableLocalesList.forEach(function (locale) {
+				if (preLoadList.indexOf(locale) === -1) {
+					preLoadList.push(locale);
+				}
+			});
+		} else {
+			var bestFitPreload = common.BestFitAvailableLocale(common.availableLocalesList, config);
+			if (bestFitPreload && preLoadList.indexOf(bestFitPreload) === -1) {
+				preLoadList.push(bestFitPreload);
+			}
+		}
+	} else if (config instanceof Array) {
+		config.forEach(function (locale) {
+			var bestFitPreload = common.BestFitAvailableLocale(common.availableLocalesList, locale);
+			if (bestFitPreload && preLoadList.indexOf(bestFitPreload) === -1) {
+				preLoadList.push(bestFitPreload);
+			}
+		});
+	} else if (config instanceof RegExp) {
+		common.availableLocalesList.forEach(function (locale) {
+			if (config.test(locale) && preLoadList.indexOf(locale) === -1) {
+				preLoadList.push(locale);
+			}
 		});
 	}
 
-	locales.preLoadList.push(common.DefaultLocale());
-	loadLocaleElements(common.DefaultLocale());
+	// Compute dependencies to require() before this plugin resolves:
+	// For every locale, load "currencies", "numbers", and "ca-gregorian".
+	var dependencies = [];
+	preLoadList.forEach(function (locale) {
+		jsonElements.forEach(function (element) {
+			dependencies.push("requirejs-text/text!./cldr/" + locale + "/" + element + ".json");
+		});
+	});
 
-	if (module.config()) {
-		if (typeof module.config() === "string") {
-			if (module.config() === "allAvailable") {
-				common.availableLocalesList.forEach(function (locale) {
-					if (locales.preLoadList.indexOf(locale) === -1) {
-						locales.preLoadList.push(locale);
-						loadLocaleElements(locale);
-					}
+
+	return {
+		load: function (path, require, onload) {
+
+			require(dependencies, function () {
+				var deps = Array(arguments);
+
+				// Return value from this plugin.
+				var locales = {
+				};
+
+				// Now that we've loaded everything all the JSON files we need,
+				// stick the results into the locales hash.
+				preLoadList.forEach(function (locale) {
+					var l = locales[locale] = {};
+					jsonElements.forEach(function (element) {
+						l[element] = JSON.parse(deps.shift());
+					});
 				});
-			} else {
-				var bestFitPreload = common.BestFitAvailableLocale(common.availableLocalesList, module.config());
-				if (bestFitPreload && locales.preLoadList.indexOf(bestFitPreload) === -1) {
-					locales.preLoadList.push(bestFitPreload);
-					loadLocaleElements(bestFitPreload);
-				}
-			}
-		} else if (module.config() instanceof Array) {
-			module.config().forEach(function (locale) {
-				var bestFitPreload = common.BestFitAvailableLocale(common.availableLocalesList, locale);
-				if (bestFitPreload && locales.preLoadList.indexOf(bestFitPreload) === -1) {
-					locales.preLoadList.push(bestFitPreload);
-					loadLocaleElements(bestFitPreload);
-				}
-			});
-		} else if (module.config() instanceof RegExp) {
-			common.availableLocalesList.forEach(function (locale) {
-				if (module.config().test(locale) && locales.preLoadList.indexOf(locale) === -1) {
-					locales.preLoadList.push(locale);
-					loadLocaleElements(locale);
-				}
+
+				onload(locales);
 			});
 		}
-	}
-	return locales;
+	};
 });
