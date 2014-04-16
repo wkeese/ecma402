44a68ed7b4083c90ac252f961e3b0be29452d0e4
Bill Keese <bill@dojotoolkit.org>
2014-04-16 11:51:39 +0900

    more fixes to plugin

--- locales.js
+++ locales.js
@@ -6,27 +6,30 @@ define(["module", "require", "./common"], function (module, require, common) {
 
 	// Compute locales to pre-load, and start building data structure this plugin will return:
 	// a hash with a key for each pre-load locale.
-	var localeData = {
-		root: {}
-	};
+	var localeData = {};
+	localeData.root = {};
 	localeData[common.DefaultLocale()] = {};
 
 	// Add each locale specified in this module's configuration.
 	var config = module.config();
 	if (config) {
-		if (typeof config === "string") {
-			config = [ config ];
-		} else if (config instanceof RegExp) {
-			config = common.availableLocalesList.filter(function (locale) {
-				return config.test(locale);
+		if (config instanceof RegExp) {
+			common.availableLocalesList.forEach(function (locale) {
+				if (config.test(locale)) {
+					localeData[locale] = {};
+				}
+			});
+		} else {
+			if (typeof config === "string") {
+				config = [ config ];
+			}
+			config.forEach(function (locale) {
+				var bestFitPreload = common.BestFitAvailableLocale(common.availableLocalesList, locale);
+				if (bestFitPreload) {
+					localeData[bestFitPreload] = {};
+				}
 			});
 		}
-		config.forEach(function (locale) {
-			var bestFitPreload = common.BestFitAvailableLocale(common.availableLocalesList, locale);
-			if (bestFitPreload) {
-				localeData[bestFitPreload] = {};
-			}
-		});
 	}
 
 	// Array of pre-load locales
